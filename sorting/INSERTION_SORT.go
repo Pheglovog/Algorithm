b4af05e42767bd3ff99290586d9d05a2220d9238
Pheglovog <3042569263@qq.com>
2024-06-20 19:51:28 +0800

    modify insertion_sort.go

--- sorting/INSERTION_SORT.go
+++ sorting/INSERTION_SORT.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	s := []int{12, 32, 53, 16, 3, 1, 720, 832, 64, 88}
+	insertionSort(s)
+	fmt.Println(s)
+}
+
+func insertionSort(s []int) {
+	for i := 1; i < len(s); i++ {
+		key := s[i]
+		j := i - 1
+		for j >= 0 && s[j] > key {
+			s[j+1] = s[j]
+			j--
+		}
+		s[j+1] = key
+	}
 }
